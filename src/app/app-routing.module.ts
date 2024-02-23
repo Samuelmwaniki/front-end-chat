48954d9b6910a99d0cf26fcc14eb77ece0a5b80d
Samuelmwaniki <smwaniki549@gmail.com>
2024-02-23 11:50:47 +0300

    adding the register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+import { RegisterComponent } from './pages/register/register.component'; // Import your register component
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent }, // Define route for register component
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/login' }, // Redirect any unknown routes to login
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
