90984707a4a3909aa2f2155af1cc31b42361f229
Samuelmwaniki <smwaniki549@gmail.com>
2024-02-29 21:21:37 +0300

    routing the verification page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component'; // Import your register component
 import { ChatsComponent } from './chats/chats.component';
+import { VerificationComponent } from './pages/verification/verification.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path:'chat',component: ChatsComponent},
+ {path:'verification',component: VerificationComponent},
   { path: 'register', component: RegisterComponent }, // Define route for register component
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
   { path: '**', redirectTo: '/login' }, // Redirect any unknown routes to login
