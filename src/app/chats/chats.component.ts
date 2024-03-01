6d6f7c615ca60f943c0d2b04ba08867d0cc97be7
Samuelmwaniki <smwaniki549@gmail.com>
2024-03-01 17:13:59 +0300

    refactorring to display users based on user id on front end

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Router } from '@angular/router';
 
-interface Message {
-  sender: string;
-  content: string;
-}
 
 interface User {
-  id: number;
+  _id: string;
   username: string;
+}
+
+interface Message {
+  _id: number;
+  sender: User;
+  recepient: User;
+  chat: string;
+  timestamp: Date;
 }
 
 @Component({
@@ -20,10 +24,12 @@ interface User {
 export class ChatsComponent implements OnInit {
   messages: Message[] = [];
   newMessage: string = '';
+  currentUser: User = {} as any;
   users: User[] = [];
-  selectedUser: number | null = null;
+  selectedUserId: string = '';
+  selectedUser: User = {} as any;
 
-  constructor(private apiService: ApiService, private router:Router) { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
     // Fetch initial messages from sessionStorage when component initializes
@@ -33,42 +39,61 @@ export class ChatsComponent implements OnInit {
     }
 
     const token = localStorage.getItem("token");
-    if (!token){
+    if (!token) {
       this.router.navigateByUrl('/login')
     }
 
-    // Fetch users for dropdown menu
-  //   this.apiService.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  
-  //   });
-  // }
-    const user = localStorage.getItem("currentUser")
-    if (user){
-        this.getUsers(user)
-    }
-
-  
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser") || '');
+    
+    this.getUsers()
   }
 
- async getUsers(user: string){
-  const currentUser = JSON.parse(user);
-   const res: any = await this.apiService.get('users');
-   console.log('users: ', res)
-    this.users = res.filter((user: any) => user._id !== currentUser._id)
-    console.log('users 2: ', this.users)
+  async getUsers() {
+    if (this.currentUser) {
+      const res: any = await this.apiService.get('users');
+      this.users = res.filter((user: any) => user._id !== this.currentUser._id);
+    } else {
+      console.log("Could not find current user");
+    }
   }
 
   sendMessage(): void {
+    console.log('Selected ', this.selectedUser, this.currentUser)
     if (this.newMessage.trim() !== '' && this.selectedUser !== null) {
       // Simulate sending message to backend and receiving response
-      const newMessage: Message = { sender: 'Me', content: this.newMessage };
+      const newMessage: Message = { 
+        sender: this.currentUser, 
+        recepient: this.selectedUser,
+        _id: (new Date()).getUTCDate(),
+        chat: this.newMessage,
+        timestamp: new Date(),
+      };
       this.messages.push(newMessage);
 
       // Store updated messages in sessionStorage
       sessionStorage.setItem('messages', JSON.stringify(this.messages));
 
       this.newMessage = ''; // Clear input field after sending message
+    }
+  }
+
+  onSelectedUserChanged(){
+    if(this.selectedUserId) {
+      this.selectedUser = this.users.find((user: User) => user._id === this.selectedUserId) as any;
+    } else {
+      this.selectedUser = {} as any;
+    }
+  }
+
+  goToLogin() {
+    this.router.navigateByUrl('/login');
+  }
+
+  indexIsEven(index: any) {
+    if (typeof(index) === "number"){
+      return (index%2 === 0)
+    } else {
+      return false
     }
   }
 }
